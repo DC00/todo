50996cca1e90bd61d3e37ce371c550149a144667
DC00 <djc4ku@virginia.edu>
2019-02-19 19:12:57 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express'),
+    config = require('./config'),
+    port = process.env.port || config.PORT,
+    app = require('./app');
+
+const CREDS = require('./creds');
+const DB_URL = 
+
